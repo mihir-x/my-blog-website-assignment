e328ca3dd67b2203d754c8478f341c803cf6d14e
mihir-x <ictlancerx@gmail.com>
2023-11-05 01:26:43 +0600

    set up firebase configuration and created auth provider

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,25 @@
+import { createContext } from "react";
+import PropTypes from 'prop-types';
+
+
+export const AuthContext = createContext()
+
+const AuthProvider = ({children}) => {
+
+
+    const authInfo = {
+
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
+
+AuthProvider.propTypes = {
+    children: PropTypes.node
+}
