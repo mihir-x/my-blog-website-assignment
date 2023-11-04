ed1b48575746b2ad2f2aede1bd4e58b92afb68ff
mihir-x <ictlancerx@gmail.com>
2023-11-05 01:39:03 +0600

    added create user login and login with google methods

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,14 +1,38 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import PropTypes from 'prop-types';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import {auth} from "../Firebase/firebase.config"
 
 
 export const AuthContext = createContext()
+const googleProvider = new GoogleAuthProvider()
 
 const AuthProvider = ({children}) => {
 
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const createUser = (email, password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const logIn = (email, password) =>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const loginWithGoogle = () =>{
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+    
 
     const authInfo = {
-
+        user,
+        loading,
+        createUser,
+        logIn,
+        loginWithGoogle,
     }
 
     return (
