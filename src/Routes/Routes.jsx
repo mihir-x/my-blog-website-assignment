506bed6f37c0a72e4b55e3f59649eaca30a1db8c
mihir-x <ictlancerx@gmail.com>
2023-11-04 20:58:11 +0600

    created login and registration routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import {
   } from "react-router-dom";
 import MainLayOut from "../LayOuts/MainLayOut";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Registration from "../Pages/Registration/Registration";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +17,14 @@ const router = createBrowserRouter([
         }
       ]
     },
+    {
+        path: '/login',
+        element: <Login></Login>
+    },
+    {
+        path: '/registration',
+        element: <Registration></Registration>
+    }
   ]);
 
 export default router
