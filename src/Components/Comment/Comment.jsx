7a2ff85125d14bb243fa7d76d486c1fd4c670360
mihir-x <ictlancerx@gmail.com>
2023-11-06 02:57:14 +0600

    fixed comment image size

--- src/Components/Comment/Comment.jsx
+++ src/Components/Comment/Comment.jsx
@@ -1,17 +1,15 @@
 import PropTypes from 'prop-types';
 
-const Comment = ({comment}) => {
+const Comment = ({ comment }) => {
 
-    const {name, photo, commentText} = comment
+    const { name, photo, commentText } = comment
 
     return (
-        <div className='flex gap-4 mt-5 md:mt-7'>
-            <div>
-                <figure><img src={photo} alt="" className='h-10 w-10 md:h-16 md:w-16 rounded-full' /></figure>
-            </div>
+        <div className='flex gap-1 md:gap-4 mt-5 md:mt-7'>
+            <img src={photo} alt="" className='h-10 w-10 md:h-16 md:w-16 rounded-full' />
             <div className='space-y-1 bg-slate-200 rounded-xl p-4'>
                 <h1 className='text-lg font-bold'>{name}</h1>
-                <p>{commentText}</p>
+                <p className='text-xs md:text-sm'>{commentText}</p>
             </div>
         </div>
     );
