9db1032c5ccc6a95314806bd6fe224933e370c42
mihir-x <ictlancerx@gmail.com>
2023-11-06 02:37:42 +0600

    fixed comment image size

--- src/Components/Comment/Comment.jsx
+++ src/Components/Comment/Comment.jsx
@@ -7,7 +7,7 @@ const Comment = ({comment}) => {
     return (
         <div className='flex gap-4 mt-5 md:mt-7'>
             <div>
-                <figure><img src={photo} alt="" className='h-16 w-16 rounded-full' /></figure>
+                <figure><img src={photo} alt="" className='h-10 w-10 md:h-16 md:w-16 rounded-full' /></figure>
             </div>
             <div className='space-y-1 bg-slate-200 rounded-xl p-4'>
                 <h1 className='text-lg font-bold'>{name}</h1>
