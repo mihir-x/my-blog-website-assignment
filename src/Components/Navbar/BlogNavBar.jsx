c441615da93e48b916e4064fa9b25c0de5326eb2
mihir-x <ictlancerx@gmail.com>
2023-11-05 11:37:12 +0600

    added logged user info on navbar

--- src/Components/Navbar/BlogNavBar.jsx
+++ src/Components/Navbar/BlogNavBar.jsx
@@ -1,15 +1,34 @@
-import { Avatar, Dropdown, Navbar } from 'flowbite-react';
-import { NavLink } from 'react-router-dom';
+import { Avatar, Button, Dropdown, Navbar } from 'flowbite-react';
+import { useContext } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { AuthContext } from '../../Provider/AuthProvider';
+import swal from 'sweetalert';
 
 const BlogNavBar = () => {
+    const { user, loading, logOut } = useContext(AuthContext)
+    console.log(user)
+    // console.log(user.photoURL)
 
     const navLinks = <>
-            <NavLink to='/' className={({isActive}) => isActive? 'font-bold underline text-green-500':''}>Home</NavLink>
-            <NavLink to='/addblog' className={({isActive}) => isActive? 'font-bold underline text-green-500':''}>Add Blog</NavLink>
-            <NavLink to='/allblogs' className={({isActive}) => isActive? 'font-bold underline text-green-500':''}>All Blogs</NavLink>
-            <NavLink to='/featuredblogs' className={({isActive}) => isActive? 'font-bold underline text-green-500':''}>Featured Blogs</NavLink>
-            <NavLink to='/wishlist' className={({isActive}) => isActive? 'font-bold underline text-green-500':''}>Wishlist</NavLink>
+        <NavLink to='/' className={({ isActive }) => isActive ? 'font-bold underline text-green-500' : ''}>Home</NavLink>
+        <NavLink to='/addblog' className={({ isActive }) => isActive ? 'font-bold underline text-green-500' : ''}>Add Blog</NavLink>
+        <NavLink to='/allblogs' className={({ isActive }) => isActive ? 'font-bold underline text-green-500' : ''}>All Blogs</NavLink>
+        <NavLink to='/featuredblogs' className={({ isActive }) => isActive ? 'font-bold underline text-green-500' : ''}>Featured Blogs</NavLink>
+        <NavLink to='/wishlist' className={({ isActive }) => isActive ? 'font-bold underline text-green-500' : ''}>Wishlist</NavLink>
     </>
+
+    if (loading) {
+        return <p>loading</p>
+    }
+    const handleLogout = () => {
+        logOut()
+            .then(() => {
+                swal('See Ya!', 'User logOut successful', 'success')
+            })
+            .catch(err => {
+                swal('Ooops!', err.message, 'error')
+            })
+    }
 
     return (
         <div className='shadow-xl'>
@@ -19,23 +38,28 @@ const BlogNavBar = () => {
                     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
                 </Navbar.Brand>
                 <div className="flex md:order-2">
-                    <Dropdown
-                        arrowIcon={false}
-                        inline
-                        label={
-                            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
-                        }
-                    >
-                        <Dropdown.Header>
-                            <span className="block text-sm">Bonnie Green</span>
-                            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
-                        </Dropdown.Header>
-                        <Dropdown.Item>Dashboard</Dropdown.Item>
-                        <Dropdown.Item>Settings</Dropdown.Item>
-                        <Dropdown.Item>Earnings</Dropdown.Item>
-                        <Dropdown.Divider />
-                        <Dropdown.Item>Sign out</Dropdown.Item>
-                    </Dropdown>
+                    {
+                        user ? <Dropdown
+                            arrowIcon={false}
+                            inline
+                            label={
+                                <Avatar alt="User settings" img={user.photoURL} rounded />
+                            }
+                        >
+                            <Dropdown.Header>
+                                <span className="block text-sm">{user.displayName}</span>
+                                <span className="block truncate text-sm font-medium">{user.email}</span>
+                            </Dropdown.Header>
+                            <Dropdown.Item>Dashboard</Dropdown.Item>
+                            <Dropdown.Item>Settings</Dropdown.Item>
+                            <Dropdown.Item>Earnings</Dropdown.Item>
+                            <Dropdown.Divider />
+                            <Dropdown.Item><Button onClick={handleLogout} color="failure">Logout</Button></Dropdown.Item>
+                        </Dropdown> : <div className='md:flex gap-5'>
+                            <Link to='/login'><Button gradientDuoTone="purpleToBlue">Login</Button></Link>
+                            <Link to='/registration'><Button gradientDuoTone="purpleToBlue">Register</Button></Link>
+                        </div>
+                    }
                     <Navbar.Toggle />
                 </div>
                 <Navbar.Collapse>
