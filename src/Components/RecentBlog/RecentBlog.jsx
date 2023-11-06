fe4d2ea84e27d91db3111ec128ee5181933eedeb
mihir-x <ictlancerx@gmail.com>
2023-11-06 21:57:19 +0600

    added recent blogs section in home page

--- src/Components/RecentBlog/RecentBlog.jsx
+++ src/Components/RecentBlog/RecentBlog.jsx
@@ -16,7 +16,7 @@ const RecentBlog = () => {
     return (
         <div className="max-w-screen-lg mx-auto">
             <h1 className="text-xl md:text-3xl lg:text-5xl font-bold text-center mt-5 md:mt-10">Recent Blogs</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5 mt-5 md:mt-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5 my-5 md:my-10 ">
                 {
                     recentBlogs?.map(blog => <BlogCard key={blog._id} blog={blog}></BlogCard>)
                 }
